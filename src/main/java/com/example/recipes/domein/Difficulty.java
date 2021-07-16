0e2be12dac61bac3251f4e300beef518065d5c35
mathieu8 <mathieuvanommeren@gmail.com>
2021-07-16 14:37:00 +0200

    stolen from john

--- src/main/java/com/example/recipes/domein/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package com.example.recipes.domein;
-
-public enum Difficulty {
-    EASY, MEDIUM, HARD
-}
--- src/main/java/com/example/recipes/repositories/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipes.repositories;
-
-import com.example.recipes.domein.Recipe;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-}
